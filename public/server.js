f272f56e3368a1c31f2ba7bad1bf2bdd44ce071a
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-11-29 16:15:21 -0700

    created routes

--- public/server.js
+++ public/server.js
@@ -6,6 +6,7 @@ const uuid = require('../helpers/uuid');
 
 // require express
 const express = require('express');
+const { Z_ASCII } = require('zlib');
 // const { title } = require('process');
 
 const PORT = process.env.PORT || 3001;
@@ -18,3 +19,66 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) => 
+    fs.writeFile(destination, JSON.stringify(content), (err) => 
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if(err) {
+            console.erroe(err);
+        }
+        else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+}
+
+app.get("api/notes", (req, res) => {
+    console.info(`${req.method} recieved for notes`);
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+});
+
+app.post("api/notes", (req, res) => {
+    console.info(`${req.method} request recieved to add notes`);
+    const { text, title } = req.body;
+
+    if(title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        
+        readAndAppend(newNote, "./db/db.json");
+
+        const response = {
+            status: 'success', 
+            body: newNote,
+        };
+
+        res.status(201).json(response);
+    }
+    else {
+        res.status(500).json('error: could not add note')
+}
+});
+
+// listener on port 3001
+app.listen( PORT, () =>
+    console.log(`Express server listening on port ${PORT}!`)
+);
